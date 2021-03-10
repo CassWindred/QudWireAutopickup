29bc1cbcd0f9b93e3af59ffa6464e88c34994508
Cass <tomcasswindred@gmail.com>
2021-03-10 16:34:53 +0000

    Removed debug messages

--- CopperWireAutoPickup/GameObjectHarmonyPatch.cs
+++ CopperWireAutoPickup/GameObjectHarmonyPatch.cs
@@ -14,17 +14,17 @@ namespace Fyrefly.CopperWireAutoPickup.HarmonyPatches
         [HarmonyPatch("ShouldAutoget")]
         static void Postfix(ref bool __result, ref GameObject __instance)
         {
-            MessageQueue.AddPlayerMessage($"Running Autoget Postfix on {__instance.DisplayName}");
+            //MessageQueue.AddPlayerMessage($"Running Autoget Postfix on {__instance.DisplayName}");
             if (Options.AutogetWire != "Never")
             {
-                MessageQueue.AddPlayerMessage($"Options not set to Never");
+                //MessageQueue.AddPlayerMessage($"Options not set to Never");
                 if ((XRLCore.Core.Game.HasUnfinishedQuest("Weirdwire Conduit.. Eureka!")
                     && !XRLCore.Core.Game.HasFinishedQuestStep("Weirdwire Conduit.. Eureka!", "Find 200 feet of copper wire"))
                     | Options.AutogetWire == "Always")
                 {
-                    MessageQueue.AddPlayerMessage($"Either has unfinished quest or option set to Always");
+                    //MessageQueue.AddPlayerMessage($"Either has unfinished quest or option set to Always");
                     if (__instance.HasPart("Wire") && ((Wire)__instance.GetPart("Wire")).Length>=Options.AutogetWireMinLength) {
-                        MessageQueue.AddPlayerMessage($"Wire detected, returning true!");
+                        //MessageQueue.AddPlayerMessage($"Wire detected, returning true!");
                         __result = true;
                     }
                 }
